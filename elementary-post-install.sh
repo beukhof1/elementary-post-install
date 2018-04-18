e913c6b01e4fb29b6bc0e77a42d1c9f41984463d
Kirill Romanov <djaler1@gmail.com>
2018-04-18 21:51:16 +0300

    Sublime Text из Snap

--- elementary-post-install.sh
+++ elementary-post-install.sh
@@ -22,9 +22,6 @@ echo "Add repository for Simple Screen Recorder"
 sudo add-apt-repository -y --no-update ppa:maarten-baert/simplescreenrecorder
 echo "Add repository for Bomi"
 sudo add-apt-repository -y --no-update ppa:nemonein/tailored
-echo "Add repository for Sublime Text 3"
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo sh -c 'echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list'
 #echo "Add repository for Insomnia"
 #wget -qO - https://insomnia.rest/keys/debian-public.key.asc | sudo apt-key add -
 #sudo sh -c 'echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" > /etc/apt/sources.list.d/insomnia.list'
@@ -47,8 +44,6 @@ echo "Install Java 8"
 sudo debconf-set-selections <<< 'oracle-java8-installer shared/accepted-oracle-license-v1-1 select true'
 sudo debconf-set-selections <<< 'oracle-java8-installer shared/accepted-oracle-license-v1-1 seen true'
 sudo apt-get install -y oracle-java8-installer
-echo "Install Sublime Text 3"
-sudo apt-get install -y sublime-text
 #echo "Install Insomnia"
 #sudo apt-get install -y insomnia
 echo "Install Bleachbit"
@@ -118,6 +113,8 @@ echo "Install PyCharm"
 sudo snap install pycharm-professional --classic
 echo "Install Slack"
 sudo snap install slack --classic
+echo "Install Sublime Text"
+sudo snap install sublime-text --classic
 echo
 
 echo "*** Uninstall packages ***"
